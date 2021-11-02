297d40996cd5096ea61a39ac8543b11daee12346
Miguel Hernández <miguelhernandezidarraga@gmail.com>
2021-11-02 13:41:24 -0500

    actualizado

--- script.js
+++ script.js
@@ -11,38 +11,41 @@ const cart = document.getElementById('cart');
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    cargarData(url);
+  cargarData(url);
 })
 
 //Traer la data y recorrer el array y cambiar los contenidos de los atributtos
 //Clonar la template-card y adjuntarala como nodo al Fragment, y luego a cards
-const cargarData = async(url) =>{
-    const resp = await fetch(url); //GET
-    const products = await resp.json();
-    console.log(products);
-    products.forEach(product => {
-        const {id, title, image, price} = product;
-        templateCard.querySelector('img').setAttribute('src', image);
-        templateCard.querySelector('h3').textContent = title;
-        templateCard.querySelector('h4').textContent = `$ ${price}`;
-        templateCard.querySelector('button').dataset.id = id;
-        templateCard.querySelector('.product-image').dataset.id = id;
-        const node = templateCard.cloneNode(true);
-        fragment.appendChild(node);
-    })
-    cards.appendChild(fragment);
+const cargarData = async (url) => {
+  const resp = await fetch(url); //GET
+  const products = await resp.json();
+  console.log(products);
+  products.forEach(product => {
+    const { id, title, image, price } = product;
+    templateCard.querySelector('img').setAttribute('src', image);
+    templateCard.querySelector('h3').textContent = title;
+    templateCard.querySelector('h4').textContent = `$ ${price}`;
+    templateCard.querySelector('button').dataset.id = id;
+    templateCard.querySelector('.product-image').dataset.id = id;
+    
+    
+    
+    const node = templateCard.cloneNode(true);
+    fragment.appendChild(node);
+  })
+  cards.appendChild(fragment);
 }
 
 let boton = document.getElementById("btnBuscar");
 
-boton.addEventListener('click', async(e) => {
+boton.addEventListener('click', async (e) => {
   e.preventDefault();
   const resp = await fetch(url);
   const data = await resp.json();
   const texto = document.getElementById("search").value;
   let busqueda = data.filter(item => item.title == texto || item.category == texto);
   console.log(busqueda)
-  busqueda.forEach(product =>{
+  busqueda.forEach(product => {
     const { id, title, image, price } = product;
     templateCard.querySelector("img").setAttribute("src", image);
     templateCard.querySelector("h3").textContent = title;
@@ -58,24 +61,29 @@ boton.addEventListener('click', async(e) => {
 
 let boton2 = document.getElementById("btnBuscar2");
 
-boton2.addEventListener('click', async(e) =>{
+boton2.addEventListener('click', async (e) => {
   e.preventDefault();
   const resp = await fetch(url);
   const data = await resp.json();
   let busqueda = data.filter((item) => item.category == "Comestibles");
-   busqueda.forEach((product) => {
-     const { id, title, image, price } = product;
-     templateCard.querySelector("img").setAttribute("src", image);
-     templateCard.querySelector("h3").textContent = title;
-     templateCard.querySelector("h4").textContent = `$ ${price}`;
-     templateCard.querySelector("button").dataset.id = id;
-     templateCard.querySelector(".product-image").dataset.id = id;
-     const node = templateCard.cloneNode(true);
-     fragment.appendChild(node);
-   });
-   cards.innerHTML = "";
-   cards.appendChild(fragment);
+  busqueda.forEach((product) => {
+    const { id, title, image, price } = product;
+    templateCard.querySelector("img").setAttribute("src", image);
+    
+
+
+    templateCard.querySelector("h3").textContent = title;
+    templateCard.querySelector("h4").textContent = `$ ${price}`;
+    templateCard.querySelector("button").dataset.id = id;
+    templateCard.querySelector(".product-image").dataset.id = id;
+    const node = templateCard.cloneNode(true);
+    fragment.appendChild(node);
+  });
+  cards.innerHTML = "";
+  cards.appendChild(fragment);
+ 
 })
+
 
 
 
@@ -101,7 +109,7 @@ cards.addEventListener("click", (e) => {
 });
 
 
-/* 
+/*
 //Cuando se click a una imagen del producto
 //Se lleva ese producto al localStorage y se llama la funcion getProduct que trae el detalle del producto
 cards.addEventListener("click", (e) => {
@@ -150,3 +158,16 @@ cart.addEventListener("click", (e) =>{
     cartPage.appendChild(fragment);
 })
  */
+
+//CREANDO MODAL
+
+cards.addEventListener("click", async(e) =>{
+    e.preventDefault()
+    let datos = await fetch(url)
+    let resp = await datos.json()
+    console.log(resp)
+})
+
+
+
+
