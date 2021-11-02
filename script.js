c4d25bd65893780b3944ee42cb1d2ff1d2091071
Jeisson Villamarin <jeissonvillamarin1@gmail.com>
2021-11-02 17:19:00 -0500

    Last changes

--- script.js
+++ script.js
@@ -76,6 +76,16 @@ cards.addEventListener('click', async(e) =>{
   })
 })
 
+// Carrito
 
+const btnCarrito = document.getElementById("menuImg");
+
+btnCarrito.addEventListener("click", (e) => {
+  document.getElementById("contenedorSudmenu").style.display = "block";
+});
+
+btnCarrito.addEventListener("mouseenter", (e) => {
+  document.getElementById("contenedorSudmenu").style.display = "none";
+});
 
 
